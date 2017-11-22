c3eb9825b02189488f4d220827b620b8804c1dea
dedmau5 <ekendahl@gmail.com>
2017-11-22 11:23:56 +0100

    added UAC disable for win10

---
+++ UAC.ps1
@@ -0,0 +1,15 @@
+﻿$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+Set-ItemProperty -Path $registryPath -Name "ConsentPromptBehaviorAdmin" -Value 5 -type dword
+Set-ItemProperty -Path $registryPath -Name "ConsentPromptBehaviorUser" -Value 3 -type dword
+Set-ItemProperty -Path $registryPath -Name "EnableInstallerDetection" -Value 1 -type dword
+Set-ItemProperty -Path $registryPath -Name "EnableLUA" -Value 1 -type dword
+Set-ItemProperty -Path $registryPath -Name "EnableVirtualization" -Value 1 -type dword
+Set-ItemProperty -Path $registryPath -Name "PromptOnSecureDesktop" -Value 1 -type dword
+Set-ItemProperty -Path $registryPath -Name "ValidateAdminCodeSignatures" -Value 0 -type dword
+Set-ItemProperty -Path $registryPath -Name "FilterAdministratorToken" -Value 0 -type dword
+
+$wshell = New-Object -ComObject wscript.shell;
+$wshell.AppActivate('Windows PowerShell')
+Sleep 1
+$wshell.SendKeys('~')
+
