a358d93ba4aafc0dff3a94ded4473dc007d87a7a
Joar Ekendahl <ekendahl@gmail.com>
2018-04-19 13:44:36 +0200

    first commit of MappaDisc
    added MappaDisc for easier mapping of network drives

---
+++ mappaDisc.bat
@@ -0,0 +1,10 @@
+net use u: /delete
+net use v: /delete
+net use w: /delete
+net use t: /delete
+net use z: /delete
+net use u: \\ltt211.rsva.se\loadrunner01$ /persistent:yes
+net use v: \\ltt211.rsva.se\loadrunner02$ /persistent:yes
+net use w: \\ltt211.rsva.se\loadrunner03$ /persistent:yes 
+net use t: \\ltt211.rsva.se\loadrunner$ /persistent:yes 
+net use z: \\ltt211.rsva.se\loadrunner_test$ /persistent:yes
