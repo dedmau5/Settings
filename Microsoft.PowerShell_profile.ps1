8d9f3fdea80048e53ade26ed1cb1d849d5e34a23
Joar Ekendahl <ekendahl@gmail.com>
2024-06-04 15:38:05 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,28 +1,16 @@
+
+Import-Module posh-git
+
+# function GitStatus {
+#   git status 
+# }
+# Set-Alias gs GitStatus
+
 # Notepad $profile to open your profile file
 # Load posh-git example profile
-. 'C:\tools\poshgit\dahlbyk-posh-git-a4faccd\profile.example.ps1' choco
+#. 'C:\tools\poshgit\dahlbyk-posh-git-a4faccd\profile.example.ps1' choco
 #load ssh-.ps1
-. 'C:\Users\1306\Documents\WindowsPowerShell\ssh-.ps1'
-
-# Created: April 07, 2017
-# Usage: touch filename
-#
-# References:
-# @link http://superuser.com/a/571154/501534
-# @link http://www.howtogeek.com/50236/customizing-your-powershell-profile/
-# @link @link http://ss64.com/ps/set-content.html (convert to UTF-8)
-function touch {
-    $file = $args[0]
-    if ($file -eq $null) {
-        throw "No filename supplied"
-    }
-    if (test-path $file) {
-        (Get-ChildItem $file).LastWriteTime = get-date
-    } else {
-        echo '' >> $file
-        (get-content $file) | set-content $file -encoding UTF8
-    }
-}
+# . 'C:\Users\a00577465\OneDrive - ONEVIRTUALOFFICE\Documents\WindowsPowerShell\profile.ps1'
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -30,12 +18,12 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
-Import-Module posh-git
-Start-SshAgent -Quiet
+# Import-Module posh-git
+# #Start-SshAgent -Quiet
 
 # Alias functions
 function Get-GitStatus { & git status $args }
-New-Alias -Name gs -Value Get-GitStatus
+Set-Alias -Name gs -Value Get-GitStatus
 
 function Get-GitPull { & git pull $args }
 New-Alias -Name gpl -Value Get-GitPull -Force -Option AllScope
